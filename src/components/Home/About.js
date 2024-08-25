38f37ec6a22af540c6aee7ab6a49d10e17a46d9a
Birendra <birendrapradhan112@gmail.com>
2024-08-25 13:31:33 +0530

    Add git hub info

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -240,8 +240,11 @@ export default function About() {
                     <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
                   </div>
                   <div className="w-11/12">
-                    <p className="text-sm">Profile informations changed.</p>
-                    <p className="text-xs text-gray-500">3 min ago</p>
+                    <a href="https://github.com/birendra-max" className="mt-4 text-blue-600 font-bold">
+                      GitHub Profile.
+                    </a>
+                    <p className="text-sm"></p>
+                    <p className="text-xs text-gray-500">Profile Changed 3 min ago</p>
                   </div>
                 </div>
                 {/* <!-- end::Timeline item --> */}
@@ -253,13 +256,13 @@ export default function About() {
                   </div>
                   <div className="w-11/12">
                     <p className="text-sm">
-                      Connected with{" "}
-                      <a href="/" className="text-blue-600 font-bold">
-                        Colby Covington
+                      Github Repo {" "}
+                      <a href="https://github.com/birendra-max/USRECHARGEREFILL.COM.git" className="text-blue-600 font-bold">
+                        USRECHARGEREFILL.COM
                       </a>
                       .
                     </p>
-                    <p className="text-xs text-gray-500">15 min ago</p>
+                    <p className="text-xs text-gray-500">Last Update Jul 7.</p>
                   </div>
                 </div>
                 {/* <!-- end::Timeline item --> */}
@@ -271,13 +274,12 @@ export default function About() {
                   </div>
                   <div className="w-11/12">
                     <p className="text-sm">
-                      Invoice{" "}
-                      <a href="/" className="text-blue-600 font-bold">
-                        #4563
-                      </a>{" "}
-                      was created.
+                      Git Repo{" "}
+                      <a href="https://github.com/birendra-max/sqltecherror.com.git" className="text-blue-600 font-bold">
+                        sqltecherror.com
+                      </a>{" a blogging site"}
                     </p>
-                    <p className="text-xs text-gray-500">57 min ago</p>
+                    <p className="text-xs text-gray-500">Last Update Jul 7.</p>
                   </div>
                 </div>
                 {/* <!-- end::Timeline item --> */}
@@ -289,31 +291,31 @@ export default function About() {
                   </div>
                   <div className="w-11/12">
                     <p className="text-sm">
-                      Message received from{" "}
+                      Git Reppo{" "}
+                      <a href="https://github.com/birendra-max/DbMigration.git" className="text-blue-600 font-bold">
+                        DbMigration
+                      </a>{" Migration App"}
+                      .
+                    </p>
+                    <p className="text-xs text-gray-500">Updated Last week</p>
+                  </div>
+                </div>
+                {/* <!-- end::Timeline item --> */}
+
+                {/* <!-- start::Timeline item --> */}
+                <div className="flex items-center w-full my-6 -ml-1.5">
+                  <div className="w-1/12 z-10">
+                    <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
+                  </div>
+                  <div className="w-11/12">
+                    <p className="text-sm">
+                      Git Repo {" "}
                       <a href="/" className="text-blue-600 font-bold">
-                        Cecilia Hendric
+                        vickyportfolio.com
                       </a>
                       .
                     </p>
-                    <p className="text-xs text-gray-500">1 hour ago</p>
-                  </div>
-                </div>
-                {/* <!-- end::Timeline item --> */}
-
-                {/* <!-- start::Timeline item --> */}
-                <div className="flex items-center w-full my-6 -ml-1.5">
-                  <div className="w-1/12 z-10">
-                    <div className="w-3.5 h-3.5 bg-blue-600 rounded-full"></div>
-                  </div>
-                  <div className="w-11/12">
-                    <p className="text-sm">
-                      New order received{" "}
-                      <a href="/" className="text-blue-600 font-bold">
-                        #OR9653
-                      </a>
-                      .
-                    </p>
-                    <p className="text-xs text-gray-500">2 hours ago</p>
+                    <p className="text-xs text-gray-500">Last Updated 12 min ago</p>
                   </div>
                 </div>
                 {/* <!-- end::Timeline item --> */}
